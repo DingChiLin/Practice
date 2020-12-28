b5c321bdd53e74d54939e08bfa44bafbd18d47ec
Arthur Lin <gn01168178@yahoo.com.tw>
2020-12-28 09:59:42 +0800

    modify DI demo code

--- unit_test_demo/refactor_demo/clean_code/depedency_injection.js
+++ unit_test_demo/refactor_demo/clean_code/depedency_injection.js
@@ -1,4 +1,6 @@
-// No Dependency Injection
+/**
+ * No Dependency Injection
+ */
 function getProductSeries1(productId) {
     let now = Date.now();
     return productId + now.toString();
@@ -7,11 +9,13 @@ function getProductSeries1(productId) {
 const productSeries1 = getProductSeries1(111);
 console.log(productSeries1);
 
-// With Dependency Injection
-function getProductSeries2(productId, now) {
-    return productId + now.toString();
-}
+/**
+ * With Dependency Injection
+ */
+// function getProductSeries2(productId, now) {
+//     return productId + now.toString();
+// }
 
-let now = Date.now();
-const productSeries2 = getProductSeries2(111, now);
-console.log(productSeries2);
+// let now = Date.now();
+// const productSeries2 = getProductSeries2(111, now);
+// console.log(productSeries2);
