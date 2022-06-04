44a45e1265c83e7823944781cc21ae594393fb1e
Arthur Lin <gn01168178@yahoo.com.tw>
2022-06-04 22:14:38 +0800

    add python OOP

---
+++ fp_test.py
@@ -0,0 +1,22 @@
+let nums = [1,2,3]
+
+let nums2 = []
+
+for (a in nums) {
+    console.log(a);
+}
+
+
+
+
+
+
+
+# let nums2 = nums.map(x => x*2)
+# console.log(nums2)
+
+# let nums3 = nums2.filter(x => x < 5)
+# console.log(nums3)
+
+# let total = nums3.reduce((res, x) => res + x, 0)
+# console.log(total)
--- test.py
+++
@@ -1,4 +0,0 @@
-def test(a: int):
-	a = a**10
-	return a
-print(test(100))
